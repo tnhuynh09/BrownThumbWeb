a8aa5ea454ae75ecbc297a7590f632132336c3b3
Emma Huynh <emmahuynh@Emmas-MBP.fios-router.home>
2020-08-18 22:26:00 -0700

    testing home

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/home')
 def show_homepage():
     return ("HOME")
     # return render_template('base.html')
